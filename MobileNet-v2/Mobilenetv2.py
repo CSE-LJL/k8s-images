71e7b74d5e953539afaee067a9542188b20c17cd
CSE_LJL <63186223+CSE-LJL@users.noreply.github.com>
2024-07-16 22:02:33 +0800

    Create Mobilenetv2.py

---
+++ MobileNet-v2/Mobilenetv2.py
@@ -0,0 +1,162 @@
+import torch
+import torch.nn as nn
+
+class ConvBNReLU(nn.Sequential):
+    def __init__(self, in_channel, out_channel, kernel_size=3, stride=1, groups=1):
+        padding = (kernel_size - 1) // 2
+        super(ConvBNReLU, self).__init__(
+            nn.Conv2d(in_channel, out_channel, kernel_size, stride, padding, groups=groups, bias=False),
+            nn.BatchNorm2d(out_channel),
+            nn.ReLU6(inplace=True)
+        )
+
+
+class InvertedResidual(nn.Module):
+    def __init__(self, in_channel, out_channel, stride, expand_ratio):
+        super(InvertedResidual, self).__init__()
+        hidden_channel = in_channel * expand_ratio
+        self.use_shortcut = stride == 1 and in_channel == out_channel
+
+        layers = []                          # 定义层列表
+        if expand_ratio != 1:
+            # 1x1 pointwise conv
+            layers.append(ConvBNReLU(in_channel, hidden_channel, kernel_size=1))
+        layers.extend([
+            # 3x3 depthwise conv
+            ConvBNReLU(hidden_channel, hidden_channel, stride=stride, groups=hidden_channel),
+            # 1x1 pointwise conv(linear)
+            nn.Conv2d(hidden_channel, out_channel, kernel_size=1, bias=False),
+            nn.BatchNorm2d(out_channel),
+        ])
+
+        self.conv = nn.Sequential(*layers)
+
+    def forward(self, x):
+        if self.use_shortcut:
+            return x + self.conv(x)
+        else:
+            return self.conv(x)
+
+
+class MobileNetV2(nn.Module):
+    def __init__(self, num_classes=1000, alpha=1.0, round_nearest=8):
+        super(MobileNetV2, self).__init__()
+        block = InvertedResidual
+        input_channel = _make_divisible(32 * alpha, round_nearest)          # 将卷积核个数调整到最接近8的整数倍数
+        last_channel = _make_divisible(1280 * alpha, round_nearest)
+
+        inverted_residual_setting = [
+            # t, c, n, s
+            [1, 16, 1, 1],
+            [6, 24, 2, 2],
+            [6, 32, 3, 2],
+            [6, 64, 4, 2],
+            [6, 96, 3, 1],
+            [6, 160, 3, 2],
+            [6, 320, 1, 1],
+        ]
+
+        features = []
+        # conv1 layer
+        features.append(ConvBNReLU(3, input_channel, stride=2))
+        # building inverted residual residual blockes
+        for t, c, n, s in inverted_residual_setting:
+            output_channel = _make_divisible(c * alpha, round_nearest)
+            for i in range(n):
+                stride = s if i == 0 else 1
+                features.append(block(input_channel, output_channel, stride, expand_ratio=t))
+                input_channel = output_channel
+        # building last several layers
+        features.append(ConvBNReLU(input_channel, last_channel, 1))
+        # combine feature layers
+        self.features = nn.Sequential(*features)
+
+        # building classifier
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.classifier = nn.Sequential(
+            nn.Dropout(0.2),
+            nn.Linear(last_channel, num_classes)
+        )
+
+        # weight initialization
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out')
+                if m.bias is not None:
+                    nn.init.zeros_(m.bias)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.ones_(m.weight)         # 初始化均值为0
+                nn.init.zeros_(m.bias)          # 初始化方差为1
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.zeros_(m.bias)
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+
+
+def _make_divisible(ch, divisor=8, min_ch=None):
+    """
+    This function is taken from the original tf repo.
+    It ensures that all layers have a channel number that is divisible by 8
+    It can be seen here:
+    https://github.com/tensorflow/models/blob/master/research/slim/nets/mobilenet/mobilenet.py
+    """
+    if min_ch is None:
+        min_ch = divisor
+    new_ch = max(min_ch, int(ch + divisor / 2) // divisor * divisor)
+    # Make sure that round down does not go down by more than 10%.
+    if new_ch < 0.9 * ch:
+        new_ch += divisor
+    return new_ch
+
+
+if __name__ == '__main__':
+    # 模型实例化
+    epochs = 10
+    batch_size = 64
+    net = MobileNetV2(num_classes=10)
+    # 构造输入层shape==[4,3,224,224]
+    cifar_path = "../data"
+    transform = transforms.Compose(
+        [
+            transforms.Resize(256),  # 将输入图像的较短边调整到 256 像素
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]
+    )
+    trainset = torchvision.datasets.CIFAR10(
+        root=cifar_path, train=True, download=True, transform=transform
+    )
+    testset = torchvision.datasets.CIFAR10(
+        root=cifar_path, train=False, download=True, transform=transform
+    )
+
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True)
+    # testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False)
+
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = optim.Adam(params=net.parameters(), lr=0.001, betas=[0.9, 0.999], eps=1e-8)
+
+    net.train()
+
+    gpu_start_time = time.time()
+    for epoch in range(epochs):
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            inputs, labels = data[0].to(device), data[1].to(device)  # 将数据移动到 GPU 上
+            optimizer.zero_grad()
+
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+            if i % 100 == 99:
+                print(f'[{epoch + 1}, {i + 1}] loss: {running_loss / 100}')
+                running_loss = 0.0
